bf3278baa1d90d40f8238b8c2907e822f4952957
walle <myeongwoo95@naver.com>
2024-08-05 12:07:51 +0900

    gateway

--- apigateway-service/src/test/java/com/example/apigatewayservice/ApigatewayServiceApplicationTests.java
+++ apigateway-service/src/test/java/com/example/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.gateway;
+package com.example.apigatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class ApigatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
