00cbb3d2aff12411e4e1558f910a2df42db94b6f
walle <myeongwoo95@naver.com>
2024-08-08 01:13:24 +0900

    :>

--- apigateway-service/src/main/java/com/example/apigatewayservice/config/SecurityConfig.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/config/SecurityConfig.java
@@ -6,12 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
-import org.springframework.web.server.WebFilter;
 
 @Configuration
 @RequiredArgsConstructor
